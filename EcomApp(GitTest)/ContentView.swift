a3822f570d3030000bb65ce58a2dd1b8144c3176
IOSOM01 <om@unolo.com>
2025-11-22 23:00:11 +0530

    I added login screen with email and password validation

--- EcomApp(GitTest)/ContentView.swift
+++ EcomApp(GitTest)/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email:String = ""
+    @State private var password:String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text("Sign In")
+                     .font(Font.largeTitle)
+                     .fontWeight(.semibold)
+                    
+                VStack(alignment:.leading,spacing: 15) {
+                    TextField("enter email", text: $email)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    
+                    
+                    TextField("enter password", text: $password)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                }
+                
+                Button {
+                    print("action")
+                } label: {
+                    Text("Sign In")
+                        .frame(width: 200,alignment: .center)
+                        .foregroundStyle(.white)
+                        .font(.system(size: 20,weight: .semibold, design: .rounded))
+                }
+                .background(Color.green)
+                .buttonStyle(.borderedProminent)
+                
+
+                
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
