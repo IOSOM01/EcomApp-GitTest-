8324804db3d6352eec381b31c13554f2252a4565
IOSOM01 <om@unolo.com>
2025-11-22 22:03:33 +0530

    Initial Commit

---
+++ EcomApp(GitTest)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EcomApp(GitTest)
+//
+//  Created by OM BHAGWAN on 22/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EcomApp(GitTest)/EcomApp_GitTest_App.swift
@@ -0,0 +1,17 @@
+//
+//  EcomApp_GitTest_App.swift
+//  EcomApp(GitTest)
+//
+//  Created by OM BHAGWAN on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EcomApp_GitTest_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
